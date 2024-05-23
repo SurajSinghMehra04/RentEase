b3957170b5977b7487b98e99485db8bf56ea9ed5
Suraj Singh Mehra <surajsinghmehra04@gmail.com>
2024-05-23 10:12:10 +0530

    RentEase

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About RentEase</h1>
+      <p className='mb-4 text-slate-700'>The RentEase project is an innovative platform designed to streamline the process of finding student accommodations. </p>
+      <p className='mb-4 text-slate-700'>
+      The objective of RentEase is to offer a centralized hub where students can effortlessly discover, assess, and communicate with accommodation options that align with their preferences. The platform incorporates features such as real-time search, detailed accommodation listings, secure communication channels, and a robust feedback system.
+      </p>
+      <p className='mb-4 text-slate-700'>RentEase stands as a transformative force in the realm of student accommodations, offering a technologically advanced and user-friendly platform that reshapes the student living experience. The project's success lies in its commitment to transparency, community building, and continuous adaptation to emerging trends and technologies.</p>
+    </div>
+  )
+}
